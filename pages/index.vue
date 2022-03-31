97352ed78a160146944714a8539d9693d3fcf762
kyosuke <kyosuke.ito@gmail.com>
2022-03-30 22:57:11 -0700

    add basic method to indev

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,32 @@
 <script>
-export default {
-  name: "IndexPage",
-};
+import Vue from "vue";
+import bookService from "@/service/book";
+
+export default Vue.extend({
+  async asyncData() {
+    const books = await bookService.fetchBooks();
+    return {
+      books,
+    };
+  },
+  data() {
+    return {
+      form: {},
+      books: [],
+    };
+  },
+  methods: {
+    async onClickAdd() {
+      await bookService.postBook({ ...this.form });
+      this.books = await bookService.fetchBooks();
+      this.form = {};
+    },
+    async onClickDelete() {
+      await bookService.deleteBook(bookId);
+      this.books = await bookService.fetchBooks();
+    },
+  },
+});
 </script>
 
 <template></template>
